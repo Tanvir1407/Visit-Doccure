738339e52b1d9c50c1bb59c33ac26c2f4859edf1
MD. Tanvir <mt14050@gmail.com>
2022-04-18 15:28:44 +0600

    login page degin done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
+ 
   return (
     <div className="header">
-      <div><img src={'https://i.ibb.co/brt9rps/logo-1.png'} alt="" /></div>
+      <div>
+        <img src={"https://i.ibb.co/brt9rps/logo-1.png"} alt="" />
+      </div>
       <div className="header-link">
         <Link to="/">Home</Link>
         <Link to="/checkout">Checkout</Link>
         <Link to="/login">Login</Link>
       </div>
-      <div >
-        <button className="header-btn">LOGIN</button>
+      <div>
+        <button className="header-btn">
+          <Link to="/login">LOGIN</Link>
+        </button>
       </div>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h1>please login</h1>
+      <div className="login-container">
+        <h2>Login Doccure</h2>
+        <form>
+          <input type="email" name="email" id="" placeholder="Email" /> <br />
+          <input
+            type="password"
+            name="password"
+            id=""
+            placeholder="Password"
+          />{" "}
+          <br />
+          <span className="reset-password-link">Forgot password ?</span> <br />
+          <input className="submit-btn" type="submit" value="Login" />
+        </form>
+        <p>
+          <span>or</span>
+        </p>
+        <div className='social-media-btn'>
+          <button className="btn-1">
+            <i class="fab fa-facebook-f"></i> Login
+          </button>
+          <button className="btn-2">
+            <i class="fab fa-google"></i> Login
+          </button>
         </div>
+      </div>
     );
 };
 
